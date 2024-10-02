60bffaceb4a462ce86a496f659211e31e0854572
oaoba <hacchi0803.rera1126@gmail.com>
2024-10-03 03:14:00 +0900

    [fix] Dockerfile of mariadb

--- srcs/requirements/mariadb/init-db.sh
+++ srcs/requirements/mariadb/init-db.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+
+start_mariadb() {
+	mariadbd &
+	MARIADB_PID=$!
+	sleep 5
+}
+
+stop_mariadb() {
+	kill "$MARIADB_PID"
+	wait "$MARIADB_PID"
+}
+
+mkdir -p /run/mysqld
+adduser mysql
+chown -R mysql:mysql /run/mysqld
+apt-get install -y mariadb-server > /dev/null 2>&1
+sed -i 's/^bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+start_mariadb
+if ! mysql -uroot -e ";" ; then
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+fi
+
+if ! mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "USE $MYSQL_DATABASE;" ; then
+	mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+	mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+	mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+fi
+stop_mariadb
+
+exec "$@"
