af97cbaea7d8cf225c6e65d0645412dae07b8def
oaoba <hacchi0803.rera1126@gmail.com>
2024-10-07 16:37:57 +0900

    [ok] wordpressの画面が出力された

--- srcs/requirements/mariadb/init-db.sh
+++ srcs/requirements/mariadb/init-db.sh
@@ -14,19 +14,18 @@ stop_mariadb() {
 mkdir -p /run/mysqld
 adduser mysql
 chown -R mysql:mysql /run/mysqld
-apt-get install -y mariadb-server > /dev/null 2>&1
 sed -i 's/^bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 start_mariadb
 if ! mysql -uroot -e ";" ; then
-	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
 fi
 
-if ! mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "USE $MYSQL_DATABASE;" ; then
-	mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-	mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-	mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+if ! mysql -uroot -p${DB_PASSWORD} -e "USE $DB_NAME;" ; then
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+	mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+	mysql -u root -e "FLUSH PRIVILEGES;"
 fi
 stop_mariadb
 
